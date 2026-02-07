e77fd2b4a10e7d2659c3e5baa04aa7a3618dd407
felixnanabonsu <felixnanabonsu@gmail.com>
2026-02-06 20:08:46 -1100

    Fix: or continue with box

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -173,11 +173,12 @@ const SignupPage = ({ setCurrentPage, onLogin }) => {
                         </button>
 
                         <div className="relative my-6">
-                            <div className="absolute inset-0 flex items-center">
-                                <div className="w-full border-t border-gray-700" />
-                            </div>
-                            <div className="relative flex justify-center text-sm">
-                                <span className="px-3 bg-gray-900/80 text-gray-500">Or continue with</span>
+                            <div className="relative flex items-center gap-5">
+                                <div className=" w-full relative flex w-full border-t border-gray-700" />
+                                <div className="w-full relative justify-center text-sm">
+                                    <span className="relative flex text-gray-500">Or continue with</span>
+                                </div>
+                                <div className="w-full relative flex border-t border-gray-700" />
                             </div>
                         </div>
                         <ContinueWithGoogle setCurrentPage={setCurrentPage} onLogin={onLogin} />
@@ -194,9 +195,9 @@ const SignupPage = ({ setCurrentPage, onLogin }) => {
                         </button>
                     </p>
                 </div>
-            </div>
+            </div >
 
-        </section>
+        </section >
     );
 };
 
